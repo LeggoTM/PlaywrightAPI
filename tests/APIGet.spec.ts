20c7ffc90549ead463e68a296c280c6d394288bf
LeggoTM <m.tanmay17@gmail.com>
2024-12-23 17:29:31 +0530

    Different ways of adding baseURL

--- tests/APIGet.spec.ts
+++ tests/APIGet.spec.ts
@@ -1,5 +1,34 @@
-import { test } from '@playwright/test';
+import { test, request, APIRequestContext } from '@playwright/test';
+
+let globalReqContext: APIRequestContext;
+
+test.beforeAll('Initialize BaseURL', async() => {
+    globalReqContext = await request.newContext({
+        baseURL: 'https://restful-booker.herokuapp.com'
+    })
+});
 
 test('API Testing GET', async ({ request }) => {
-    
+   const bookingResponse = await request.get('https://restful-booker.herokuapp.com/booking');
+   
+   console.log(await bookingResponse.json());
 });
+
+test('API Testing baseURL', async () => {
+    const apiReqContext = await request.newContext({
+        baseURL: 'https://restful-booker.herokuapp.com'
+    });
+
+    const bookingResponse = await apiReqContext.get('/booking');
+    console.log(await bookingResponse.json());
+ });
+
+ test('BaseURL from beforeAll', async () => {
+    const bookingResponse = await globalReqContext.get('/booking');
+    console.log(await bookingResponse.json());
+ });
+
+ test('API Testing GET config file', async ({ request }) => {
+    const bookingResponse = await request.get('/booking');
+    console.log(await bookingResponse.json());
+ });
